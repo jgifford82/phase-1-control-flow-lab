65dc8074594ce4e4ae52d77af07b9b0e4df5ee30
Jennifer Gifford <jgifford82@gmail.com>
2022-05-20 18:33:35 -0600

    commit tree 367f3f03755ba51681a4d0d57408afd30df8ae07

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return ('This one is on me!');
+} 
+// This else if statement got the 3rd test passing to charge $30 over 200 feet.
+  else if(feet >= 400 && feet <= 2000){
+    return ('That will be twenty bucks.');
+} 
+// This else if statement got the 4th test passing to not allow rides over 2500 feet.
+  else if(feet > 2000 && feet <= 2499){
+    return ('I will gladly take your thirty bucks.');
+}
+  else if(feet > 2500){
+    return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
+// to charge 30 bucks, tried: 
+// >= 2000
+// > 401
+// > 2000
+// >= 400 && > 2000
+// >= 400 && 2001
+// feet ('That will be twenty bucks.') && 2001)
+// feet !400 && >2000
+// feet < 2000 && feet <=2500
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === `NYC` ? `Ok, sounds good.` : `No go.`
 }
 
-function switchOnCharmFromTip(){
+// Ternary conditional reads as return "ok sounds good" if the city parameter strictly equals NYC, otherwise return "No go."
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return `Thank you so much.`
+    case 'not as generous':
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
+
+// The switch conditional is saying that if the tip parameter is "generous", return "thank you so much."
+// If the tip parameter is "not as generous," return "thank you."
+// The the tip parameter as anything else, by default, it will return "bye."
